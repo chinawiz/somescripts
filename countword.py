3d8f9fa76d380cd8c6e907ab6c9c6a6b374548e1
chinawiz <chinawiz@gmail.com>
2018-04-17 15:17:33 +0800

    Add files via upload
    基于jieba分词写的一个分析词频的脚本

---
+++ countword.py
@@ -0,0 +1,35 @@
+# coding:utf-8
+'''
+可以分析中英文词频的脚本,基于jieba分词
+'''
+
+import collections
+import re
+import sys
+from optparse import OptionParser
+
+import jieba
+import jieba.analyse
+from jieba.analyse import ChineseAnalyzer
+
+sys.path.append('../')
+analyzer = ChineseAnalyzer()
+
+
+def get_words(file):
+    with open(file) as f:
+        words_box = []
+        for line in f:
+            for word in analyzer(line):
+                if (not re.match(u'^[\u4E00-\u9FA5]', word.text)):  # ^[\u4E00-\u9FA5]中文字符
+                    # print(word.text)
+                    words_box.append(word.text)
+    return collections.Counter(words_box)
+
+
+USAGE = "usage:    python3 extract_tags_with_weight.py [file name] "
+if len(sys.argv) < 2:
+    print(USAGE)
+    sys.exit(1)
+file_name = sys.argv[1]
+print(get_words(file_name))
